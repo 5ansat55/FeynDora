316cf94462a2a4f6f146d49c8522a7d2e6475ba4
Şaban Kayış <sbnkys1@gmail.com>
2021-02-21 02:20:28 +0300

    first screen design

--- screens/FirstScreen.js
+++ screens/FirstScreen.js
@@ -1,14 +1,37 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, TextInput } from "react-native";
+import Input from "../components/Input";
+import CustomText from "../components/CustomText";
 
 const FirstScreen = () => {
+  const inputChangeHandler = (id, value, valid) => {
+    console.log(value);
+  };
   return (
-    <View>
-      <Text>First screen !</Text>
+    <View style={styles.container}>
+      <CustomText title="Define your topics" subTitle="which you learn" />
+      <Input
+        id="title"
+        label="Topic"
+        errorText="Please enter a valid topic!"
+        keyboardType="default"
+        autoCapitalize="sentences"
+        autoCorrect
+        returnKeyType="next"
+        onInputChange={inputChangeHandler}
+        required
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 export default FirstScreen;
