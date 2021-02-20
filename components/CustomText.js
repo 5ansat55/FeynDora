7b39998240553d53c1f04f46f983c639266ceb38
Şaban Kayış <sbnkys1@gmail.com>
2021-02-21 02:16:10 +0300

    custom text is used in project for general purpose

---
+++ components/CustomText.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+import Colors from "../constants/Colors";
+
+export default CustomText = (props) => {
+  return (
+    <Text style={styles.title} {...props}>
+      {props.title} <Text style={styles.subTitle}>{props.subTitle}</Text>
+    </Text>
+  );
+};
+
+const styles = StyleSheet.create({
+    title: {
+    fontFamily: "RobotoBold",
+    fontSize: 16,
+    color:Colors.thirdColor
+  },
+  subTitle:{
+    fontFamily: "RobotoRegular",
+    fontSize: 14,
+    color:Colors.secondColor
+  }
+});
