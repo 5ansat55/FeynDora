90682d3ad852ef85944b70c2e2b3119d18c14988
Şaban Kayış <sbnkys1@gmail.com>
2021-02-21 02:13:58 +0300

    add custom fonts

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 import CustomProgressSteps from "./components/ProgressSteps";
+import * as Font from "expo-font";
+import Colors from "./constants/Colors";
 
 export default function App() {
+  const [loaded] = Font.useFonts({
+    RobotoBlack: require("./assets/fonts/Roboto-Black.ttf"),
+    RobotoBlackItalic: require("./assets/fonts/Roboto-BlackItalic.ttf"),
+    RobotoBold: require("./assets/fonts/Roboto-Bold.ttf"),
+    RobotoBoldItalic: require("./assets/fonts/Roboto-BoldItalic.ttf"),
+    RobotoItalic: require("./assets/fonts/Roboto-Italic.ttf"),
+    RobotoLight: require("./assets/fonts/Roboto-Light.ttf"),
+    RobotoLightItalic: require("./assets/fonts/Roboto-LightItalic.ttf"),
+    RobotoMedium: require("./assets/fonts/Roboto-Medium.ttf"),
+    RobotoMediumItalic: require("./assets/fonts/Roboto-MediumItalic.ttf"),
+    RobotoRegular: require("./assets/fonts/Roboto-Regular.ttf"),
+    RobotoThin: require("./assets/fonts/Roboto-Thin.ttf"),
+    RobotoThinItalic: require("./assets/fonts/Roboto-ThinItalic.ttf"),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-    <View style={styles.container}>
-    <CustomProgressSteps />
-    </View>
+      <View style={styles.container}>
+        <Text style={styles.title}>FEYNDORO</Text>
+        <CustomProgressSteps />
+      </View>
     </SafeAreaView>
   );
 }
@@ -16,9 +38,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    margin:10,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 10,
+  },
+  title: {
+    fontFamily: "RobotoBlack",
+    fontSize: 20,
+    color: Colors.thirdColor,
   },
 });
