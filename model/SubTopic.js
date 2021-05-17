5581e9415823db309dd6a5df62b84e49283c9077
Şaban Kayış <sbnkys1@gmail.com>
2021-05-17 21:38:26 +0300

    basic model which we used

---
+++ model/SubTopic.js
@@ -0,0 +1,10 @@
+class SubTopic {
+    constructor(id,subTitle,done,topicId){
+        this.id = id;
+        this.subTitle = subTitle;
+        this.done = done;
+        this.topicId = topicId;
+    }
+}
+
+export default SubTopic;
---
+++ model/Topic.js
@@ -0,0 +1,11 @@
+class Topic {
+    constructor(id, title,totalTime,done) {
+      this.id=id;
+      this.title = title;
+      this.totalTime = totalTime;
+      this.done = done;
+    }
+  }
+  
+  export default Topic;
+  
